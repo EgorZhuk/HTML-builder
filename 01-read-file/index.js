587df96f0d3af88961f490f38248ce37852159e8
EgorZhuk <egorbug@gmail.com>
2022-11-07 01:25:06 +0300

    feat: 01-read-file task completed

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,1 +1,8 @@
 const fs = require('fs');
+const path = require('path');
+const filePath = path.join(__dirname, 'text.txt');
+const readFile = fs.createReadStream(filePath, 'utf-8');
+
+readFile.on('data', chunk => console.log(chunk))
+readFile.on('error', error => console.log(`Error: ${error.message}`));
+readFile.on('end',() =>{});
